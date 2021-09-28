fd478c25cb9c3f17168522cfff402d43580a364f
aldotele <aldo.telese@hotmail.it>
2021-09-28 11:07:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### many2many
+database design practice with many-to-many relationships and bridge tables
---
+++ cinema/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class MovieActor(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "movie_Actor_bridge"
