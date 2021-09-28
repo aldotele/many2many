6d66151ed0b935bd61b0ffa69b3ae61bb126a7d5
aldotele <aldo.telese@hotmail.it>
2021-09-28 18:03:08 +0200

    director table added

--- cinema/models.py
+++ cinema/models.py
@@ -15,3 +15,12 @@ class Movie(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+
+
+class Director(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    movies = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
